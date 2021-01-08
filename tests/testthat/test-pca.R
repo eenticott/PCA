025483cf4f9646d5aa10a93be4dabff6007fc64c
Euan Enticott <euanenticott@gmail.com>
2021-01-08 14:17:11 +0000

    added pca test

--- tests/testthat/test-pca.R
+++ tests/testthat/test-pca.R
@@ -5,3 +5,8 @@ test_that("multiplication works", {
 test_that("centre works", {
   expect_equal(centre(matrix(seq(1:9), nrow = 3)), centre(3 * matrix(seq(1:9), nrow = 3), T))
 })
+
+test_that("pca works",{
+  data(iris)
+  expect_equal(prcomp(iris[-5])$rotation, pca(iris[-5], F, 0.999)$pca_directions)
+})
